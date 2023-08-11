37684ebaf07941de92f77f5609dc0fc572f2bdc2
MisterConscio <saulo.costam@gmail.com>
2023-08-10 23:32:49 -0300

    Setting footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<footer>
+  <p>EOF</p>
+</footer>
+
+<style>
+  footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding-block: 1rem;
+
+    text-align: center;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-    import Header from "$lib/components/Header.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import Header from "$lib/components/Header.svelte";
   import "$lib/styles/main.css";
 </script>
 
 <Header />
 
 <slot />
+
+<Footer />
 
